cb8090d1daebb2d6f92d4c227520325b2aa41a15
dilankadharmapala <nethmini627@gmail.com>
2024-06-10 10:31:02 +0400

    Commiting bubble sort

---
+++ BubbleSort.py
@@ -0,0 +1,12 @@
+def BubbleSort(MyList):
+  MaxIndex = len(MyList)
+  counter = MaxIndex - 1
+  for i in range(0,counter):
+    for j in range(0, counter):
+      if(MyList[j] < MyList[j+1]):
+        Temp = MyList[j]
+        MyList[j] = MyList[j+1]
+        MyList[j+1] = Temp
+    counter = counter-1 #reduction of comparisons
+MyList1 = [5,3,2,4,1]
+print(BubbleSort(MyList1))

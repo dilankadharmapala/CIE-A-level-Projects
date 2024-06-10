675fc4f214b79d88a90679d6497e0d63c4ae209a
dilankadharmapala <nethmini627@gmail.com>
2024-06-10 10:55:51 +0400

    testing 3 lists

--- BubbleSort.py
+++ BubbleSort.py
@@ -9,4 +9,8 @@ def BubbleSort(MyList):
         MyList[j+1] = Temp
     counter = counter-1 #reduction of comparisons
 MyList1 = [5,3,2,4,1]
-print(BubbleSort(MyList1))
+print(BubbleSort(MyList1)) 
+MyList2 = [5,2,78,4,1]
+print(BubbleSort(MyList2)) 
+MyList3 = [5,3,2,6,1]
+print(BubbleSort(MyList3)) 
